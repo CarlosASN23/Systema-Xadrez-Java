cdc1e051fd8f402f8af62e6ea3810a18da2344b8
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-04-21 15:27:52 -0300

    Implementação das execções e tratamento de erros

---
+++ src/main/java/br/com/compassuol/xadrez/boardgame/TabuleiroExeccoes.java
@@ -0,0 +1,10 @@
+package br.com.compassuol.xadrez.boardgame;
+
+public class TabuleiroExeccoes extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public TabuleiroExeccoes(String message) {
+        super(message);
+    }
+}
