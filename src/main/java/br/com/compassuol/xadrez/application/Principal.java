1b3db4fa08bc04d0e7461b0009cf518d38ec8194
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-04-21 13:24:41 -0300

    Primeiras classes: Posicao

---
+++ src/main/java/br/com/compassuol/xadrez/application/Principal.java
@@ -0,0 +1,11 @@
+package br.com.compassuol.xadrez.application;
+
+import br.com.compassuol.xadrez.boardgame.Posicao;
+
+public class Principal {
+    public static void main(String[] args) {
+        Posicao pos = new Posicao(3,4);
+        System.out.println(pos);
+    }
+
+}
